ad3be194127af0f39245b304bbf5eeaf8849c83f
MihaArh <miha.arh06@gmail.com>
2018-12-18 18:29:19 +0100

    narejen dizajn za loginActivity

--- app/src/main/java/com/example/aljaz/tutor4u/LoginActivity.java
+++ app/src/main/java/com/example/aljaz/tutor4u/LoginActivity.java
@@ -2,8 +2,13 @@ package com.example.aljaz.tutor4u;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    EditText nameText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
